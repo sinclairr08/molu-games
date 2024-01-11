bc3869212e9931b7c1578a47497b3e91471f4357
sinclairr08 <ljhjoon00@gmail.com>
2024-01-11 23:56:06 +0900

    feat: add select quiz function

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import short from "./api/short.json";
+import select from "./api/select.json";
+
 import ShortQuiz from "./shortQuiz";
+import SelectQuiz from "./selectQuiz";
 
 function App() {
   return (
@@ -8,7 +11,10 @@ function App() {
         <h1 className="text-xl text-sky-500">Blue Archive Quiz Site</h1>
       </div>
       {short.map((shortQuiz) => (
-        <ShortQuiz {...shortQuiz} />
+        <ShortQuiz key={shortQuiz.question} {...shortQuiz} />
+      ))}
+      {select.map((selectQuiz) => (
+        <SelectQuiz key={selectQuiz.question} {...selectQuiz} />
       ))}
     </div>
   );
---
+++ src/selectQuiz.tsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+
+export interface ISelectQuiz {
+  problemNo: number;
+  question: string;
+  imgPath?: string;
+  answerList: string[];
+  answerIndex: number;
+}
+
+interface ISelectInput {
+  answerIndex: number;
+}
+
+export default function SelectQuiz(quiz: ISelectQuiz) {
+  const { register, handleSubmit } = useForm<ISelectInput>();
+  const [isAnswer, setIsAnswer] = useState<boolean | undefined>();
+
+  const isValid = (data: ISelectInput) => {
+    // 왜 === 일 때 안 되는지
+    setIsAnswer(data.answerIndex == quiz.answerIndex);
+  };
+  return (
+    <div key={quiz.question} className="pb-12">
+      <div className="flex justify-center">
+        객관식 {quiz.problemNo}: {quiz.question}
+      </div>
+      {quiz.imgPath && (
+        <div className="flex justify-center">
+          <img src={require(`/src/imgs/${quiz.imgPath}.png`)} />
+        </div>
+      )}
+      <div className="flex justify-center">
+        <form onSubmit={handleSubmit(isValid)}>
+          <select
+            className="border border-b border-black "
+            {...register("answerIndex")}
+          >
+            {quiz.answerList.map((answerItem, i) => (
+              <option value={i + 1} key={i}>
+                {answerItem}
+              </option>
+            ))}
+          </select>
+          <input type="submit" className="cursor-pointer" />
+        </form>
+      </div>
+      {isAnswer != undefined ? (
+        <div className="flex justify-center">
+          {isAnswer == true ? "성공" : "땡"}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
